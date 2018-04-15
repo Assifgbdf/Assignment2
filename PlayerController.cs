dbc828a034245ab8b49cb86eac8cc62bdb92b643
nhanquang-SIDs-214536636 <37958855+nhanquang@users.noreply.github.com>
2018-04-15 23:41:05 +1000

    Add files via upload
    fix maps

---
+++ PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Animator anim;
+    private bool PlayMoving;
+    private Vector2 lastMove;
+    public float moveSpeed; // set up Speed of the character which can change the speed of character in Inspector > Move Speed
+                            // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayMoving = false;
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            PlayMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+
+
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            PlayMoving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+
+        }
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayMoving", PlayMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+    }
+}
+

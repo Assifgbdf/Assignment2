b85d81c0c36ada31a507525897ec9a0e969fae78
nhanquang-SIDs-214536636 <37958855+nhanquang@users.noreply.github.com>
2018-04-17 13:49:06 +1000

    Add files via upload
    add more collision into game

--- PlayerController.cs
+++ PlayerController.cs
@@ -7,10 +7,13 @@ public class PlayerController : MonoBehaviour {
     private bool PlayMoving;
     private Vector2 lastMove;
     public float moveSpeed; // set up Speed of the character which can change the speed of character in Inspector > Move Speed
-                            // Use this for initialization
+
+    private Rigidbody2D myRigidbody;
+        // Use this for initialization
     void Start()
     {
         anim = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,7 +22,8 @@ public class PlayerController : MonoBehaviour {
         PlayMoving = false;
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
             PlayMoving = true;
             lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
 
@@ -28,10 +32,19 @@ public class PlayerController : MonoBehaviour {
 
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
-            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical")*moveSpeed );
             PlayMoving = true;
             lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));
 
+        }
+        if(Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+            {
+            myRigidbody.velocity = new Vector2(0f, myRigidbody.velocity.y);
+        }
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, 0f);
         }
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
